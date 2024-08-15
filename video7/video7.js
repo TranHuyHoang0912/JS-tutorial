caed6ae6ebefcc7cc9f60859779a396c333aea25
vuahaitac <tranhuyhoang2k4hh@gmail.com>
2024-08-15 23:01:54 +0700

    first day

---
+++ video7/video7.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 10;
+console.log("Fifteen is " + (a + b) + " and\nnot " + (2 * a + b) + ".");
+console.log("Fifteen is " + a + b + " and\nnot " + (2 * a + b) + ".");
+
+console.log(`Fifteen is ${a + b} and
+not ${2 * a + b}.`);
+
+console.log(`in ra gia tri chuoi a ${a}`);
+
+console.log(`http:localhost/${b}/${a}`);
