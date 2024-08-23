f8bdae7671fc5909fa0d15996188b7fc6bde46df
vuahaitac <tranhuyhoang2k4hh@gmail.com>
2024-08-23 14:32:53 +0700

    == vs ===, null vs empty vs undefined

---
+++ video10/video10.js
@@ -0,0 +1,10 @@
+// null la rong nma ko chiem gia tri bong bo nho
+// empty cung la rong nma chiem gia tri trong bo nho = rong
+// undefined la ko co gia tri
+// js la 1 ngon ngu xida nen gan null hay empty cung dc, 
+// vi khi gan no se chuyen bien ban dau thanh kieu cua kieu sau
+let a = ``;
+a = 5;
+b = null;
+b = [`1`];
+console.log(typeof(a), typeof(b))
