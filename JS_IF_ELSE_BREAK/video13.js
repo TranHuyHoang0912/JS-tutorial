5dd3771f85add7b5fa69ee0041536d3ff7854ff4
vuahaitac <tranhuyhoang2k4hh@gmail.com>
2024-08-26 21:55:58 +0700

    if_else_break_continue

---
+++ JS_IF_ELSE_BREAK/video13.js
@@ -0,0 +1,8 @@
+let arr = [1,2,3,4,5]
+
+ for (let i=0;i<=arr.length;i++){
+    if(i===2){
+        continue;}
+        console.log(arr[i])
+ }
+ //continue la nhay vao 1 lenh khac tiep tuc, break la dung lai
