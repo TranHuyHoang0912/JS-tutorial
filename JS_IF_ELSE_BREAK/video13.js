c3169243831ffceb5256bfc17876811e0993fec0
vuahaitac <tranhuyhoang2k4hh@gmail.com>
2024-08-28 21:11:15 +0700

    switch case

--- JS_IF_ELSE_BREAK/video13.js
+++
@@ -1,8 +0,0 @@
-let arr = [1,2,3,4,5]
-
- for (let i=0;i<=arr.length;i++){
-    if(i===2){
-        continue;}
-        console.log(arr[i])
- }
- //continue la nhay vao 1 lenh khac tiep tuc, break la dung lai
---
+++ JS_Switch/video14.js
@@ -0,0 +1,23 @@
+let a = 3;
+switch (a) {
+    case 0:
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+  console.log(day)
+  // dung dau 2 cham neu co 2 case nma chi muon hien ra 1 ket qua
