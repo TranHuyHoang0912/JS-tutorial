45cf3ef84a424ea4981a1b018bcab926e57767e1
vuahaitac <tranhuyhoang2k4hh@gmail.com>
2024-08-30 23:10:02 +0700

    arrow function

---
+++ JS_ArrowFunction/video17.js
@@ -0,0 +1,8 @@
+function sum(a,b){
+    return a + b;
+}
+
+let sum = (a, b) =>{
+    return a + b;
+}
+//2 cai nay bang nhau
--- JS_Function/video16.js
+++
@@ -1,6 +0,0 @@
-function sum(a, b){
-    return a + b;
-}
-console.log(`sum a + b =`, sum(6,7))
- let c = sum(10,20);
-console.log(`c =`, c)
